cb435e11a4114a90197274f1608b71ec6f684411
Anatoli Huseu <avg.tolik@gmail.com>
2021-12-01 11:08:36 +0100

    feat: solve 1st day part one puzzle

---
+++ 1st-day/solution.ts
@@ -0,0 +1,15 @@
+const text = await Deno.readTextFile("./input.txt");
+
+const depths = text
+  .split('\n')
+  .map((string) => parseInt(string, 10));
+
+let result = 0;
+
+for (let i = 1; i < depths.length; i++) {
+  if (depths[i] - depths[i - 1] > 0) {
+    result++;
+  }
+}
+
+console.log(result);
