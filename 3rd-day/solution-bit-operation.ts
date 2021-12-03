e7899cf2cf9b070b19cc7bf28c8e128af63f21db
Anatoli Huseu <avg.tolik@gmail.com>
2021-12-03 10:56:18 +0100

    feat: solve 3rd day puzzle with binary ops

---
+++ 3rd-day/solution-bit-operation.ts
@@ -0,0 +1,51 @@
+const text = await Deno.readTextFile("./input.txt");
+
+const dec2bin = (number: number) => number.toString(2);
+
+const numbers = text
+  .split("\n")
+  .map((string) => parseInt(string, 2));
+
+const numbersAmount = numbers.length;
+const numberLength = Math.ceil(Math.log2(Math.max.apply(null, numbers)));
+
+const setBit = (number: number, index: number) => number | (1 << (index));
+
+const checkBit = (number: number, index: number) =>
+  (number & (1 << index)) !== 0;
+
+const counts = new Array(numberLength).fill(0);
+
+for (const number of numbers) {
+  let index = 0;
+
+  while (index < numberLength) {
+    counts[index] += checkBit(number, index);
+    index++;
+  }
+}
+
+let gammaNumber = 0;
+let epsilonNumber = 0;
+
+for (let index = 0; index < numberLength; index++) {
+  if (counts[index] > numbersAmount / 2) {
+    gammaNumber = setBit(gammaNumber, index);
+  } else {
+    epsilonNumber = setBit(epsilonNumber, index);
+  }
+}
+
+/**
+ * Binary:
+ * 110010111011
+ * 1101000100
+ * Answer:
+ * 2724524
+ */
+
+console.log("Binary:");
+console.log(dec2bin(gammaNumber));
+console.log(dec2bin(epsilonNumber));
+console.log("Answer:");
+console.log(gammaNumber * epsilonNumber);
--- 3rd-day/solution.ts
+++ 3rd-day/solution.ts
@@ -13,25 +13,25 @@ const zeroCount = new Array(numberLength).fill(0);
 
 for (const number of numbers) {
   number.forEach((digit, index) => {
-    if (digit === '1') {
+    if (digit === "1") {
       oneCount[index] += 1;
     } else {
       zeroCount[index] += 1;
     }
-  })
+  });
 }
 
 for (let i = 0; i < numberLength; i++) {
   if (oneCount[i] > zeroCount[i]) {
-    gamma += '1';
-    epsilon += '0';
-  }  else {
-    gamma += '0';
-    epsilon += '1';
+    gamma += "1";
+    epsilon += "0";
+  } else {
+    gamma += "0";
+    epsilon += "1";
   }
 }
 
 const gammaNumber = parseInt(gamma, 2);
 const epsilonNumber = parseInt(epsilon, 2);
 
-console.log(gammaNumber * epsilonNumber);
+console.log(gammaNumber, epsilonNumber);
